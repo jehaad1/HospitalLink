f8b3bec6e1dbe71487bd5c78f87ff4559f505cc3
Jehaad AL-Johani <JEHAADXZX518@gmail.com>
2024-05-02 12:42:32 +0300

    UI imporving

--- App.js
+++ App.js
@@ -1,5 +1,5 @@
 import { StatusBar } from "expo-status-bar";
-import { useState } from "react";
+import { useRef, useState } from "react";
 import {
   Button,
   Pressable,
@@ -7,6 +7,7 @@ import {
   Text,
   View,
   FlatList,
+  TextInput,
 } from "react-native";
 import * as Linking from "expo-linking";
 
@@ -23,16 +24,43 @@ async function fetchAPI() {
   return res;
 }
 
+async function login(email, password) {
+  console.log(email, password);
+  fetch(
+    "https://5012991e-8251-4261-94ef-3d5e276f1ee8-00-p5y7v5bisibe.spock.replit.dev/login",
+    {
+      method: "POST",
+      headers: {
+        "Content-Type": "application/json",
+      },
+      body: JSON.stringify({ email, password }),
+    }
+  )
+    .then((r) => r.json())
+    .then((r) => {
+      if (r.status === "ok") {
+        setIsAuthorized(true);
+        setIsLoading(false);
+      } else {
+        setIsLoading(false);
+        alert("Invalid email or password");
+      }
+    });
+}
+
 export default function App() {
   const [docs, setDocs] = useState(null);
-  const [loading, setLoading] = useState(false);
+  const [isLoading, setIsLoading] = useState(false);
+  const [isAuthorized, setIsAuthorized] = useState(false);
+  const [email, setEmail] = useState("");
+  const [password, setPassword] = useState("");
 
   async function getDocs() {
     fetchAPI()
       .then((r) => r.json())
       .then((r) => {
         setDocs(r.docs);
-        setLoading(false);
+        setIsLoading(false);
         getDocs();
       });
   }
@@ -42,7 +70,7 @@ export default function App() {
       <View
         style={{
           flexDirection: "row",
-          color: "white",
+          color: "#f0f0f0",
           backgroundColor: "#3b82f6",
           borderTopLeftRadius: item.top ? 7 : 0,
           borderTopRightRadius: item.top ? 7 : 0,
@@ -57,7 +85,7 @@ export default function App() {
             style={{
               fontSize: typeof item.distance === "string" ? 17 : 16,
               fontWeight: typeof item.distance === "string" ? "bold" : "normal",
-              color: "white",
+              color: "#f0f0f0",
             }}
           >
             {typeof item.distance === "string"
@@ -70,7 +98,7 @@ export default function App() {
             style={{
               fontSize: typeof item.distance === "string" ? 17 : 16,
               fontWeight: typeof item.distance === "string" ? "bold" : "normal",
-              color: "white",
+              color: "#f0f0f0",
               marginLeft: typeof item.distance === "string" ? 0 : 2,
             }}
           >
@@ -81,7 +109,7 @@ export default function App() {
                 <Pressable
                   onPress={() => Linking.openURL(item.location)}
                   style={{
-                    backgroundColor: "white",
+                    backgroundColor: "#f0f0f0",
                     color: "#3b82f6",
                     paddingHorizontal: 12,
                     paddingVertical: 6,
@@ -113,7 +141,7 @@ export default function App() {
             style={{
               fontSize: typeof item.distance === "string" ? 17 : 16,
               fontWeight: typeof item.distance === "string" ? "bold" : "normal",
-              color: "white",
+              color: "#f0f0f0",
             }}
           >
             {typeof item.distance === "string"
@@ -124,14 +152,30 @@ export default function App() {
       </View>
       {!item.bottom && (
         <View
-          style={{ width: 380, height: 1, backgroundColor: "white" }}
+          style={{ width: 380, height: 1, backgroundColor: "#f0f0f0" }}
         ></View>
       )}
     </>
   );
 
+  const inputRef = useRef(null);
+  const input2Ref = useRef(null);
+
   return (
-    <View style={styles.container}>
+    <Pressable
+      style={styles.container}
+      onPress={() => {
+        if (
+          isAuthorized ||
+          isLoading ||
+          !input2Ref.current ||
+          !inputRef.current
+        )
+          return;
+        inputRef.current.blur();
+        input2Ref.current.blur();
+      }}
+    >
       {docs ? (
         <>
           <View
@@ -172,25 +216,176 @@ export default function App() {
       ) : (
         <>
           <Text style={styles.title}>HospitalLink</Text>
-          <Pressable
-            onPress={() => {
-              if (loading) return;
-              setLoading(true);
-              getDocs();
-            }}
-            style={loading ? styles.loadingButton : styles.button}
-          >
-            <Text style={styles.buttonText}>
-              {loading ? "Loading.." : "Available Beds"}
-            </Text>
-          </Pressable>
+          {isAuthorized ? (
+            <>
+              <TextInput
+                ref={inputRef}
+                style={styles.input}
+                onChangeText={setEmail}
+                placeholder="employee@hospital.com"
+                keyboardType="email-address"
+                inputMode="email"
+                autoCorrect={false}
+              />
+              <TextInput
+                ref={input2Ref}
+                style={{ ...styles.input, marginTop: 20 }}
+                onChangeText={setPassword}
+                placeholder="password"
+                secureTextEntry={true}
+                autoCorrect={false}
+              />
+
+              <Pressable
+                onPress={() => {
+                  if (isLoading) return;
+                  setIsLoading(true);
+                  // validate email and password
+                  const isValidEmail = /^[^\s@]+@[^\s@]+\.[^\s@]+$/.test(email);
+                  const isValidPassword = password.length >= 6;
+                  if (!isValidEmail || !isValidPassword) {
+                    setIsLoading(false);
+                    alert("Invalid email or password");
+                    return;
+                  }
+                }}
+                style={styles.button}
+              >
+                <Text style={styles.buttonText}>
+                  {isLoading ? "Logging in.." : "Login"}
+                </Text>
+              </Pressable>
+              <View
+                style={{
+                  color: "#f0f0f0",
+                  opacity: 0.9,
+                  textAlign: "center",
+                  marginTop: 275,
+                  width: 250,
+                  flex: 1,
+                  alignItems: "center",
+                  gap: 2,
+                }}
+              >
+                <Text
+                  style={{
+                    color: "#f0f0f0",
+                    fontSize: 12,
+                  }}
+                >
+                  If you are a hospital and want to create
+                </Text>
+                <View
+                  style={{
+                    flex: 1,
+                    flexDirection: "row",
+                    justifyContent: "center",
+                  }}
+                >
+                  <Text
+                    style={{
+                      color: "#f0f0f0",
+                      fontSize: 12,
+                    }}
+                  >
+                    employee accounts, please{" "}
+                  </Text>
+                  <Pressable
+                    onPress={() =>
+                      Linking.openURL("mailto:register@hospitalink.com")
+                    }
+                    style={{
+                      color: "#f0f0f0",
+                    }}
+                  >
+                    <Text
+                      style={{
+                        color: "#3b82f6",
+                        textDecorationLine: "underline",
+                        fontSize: 12,
+                      }}
+                    >
+                      Contact Us
+                    </Text>
+                  </Pressable>
+                </View>
+              </View>
+            </>
+          ) : (
+            <>
+              <Text
+                style={{
+                  ...styles.title,
+                  marginTop: 100,
+                  width: 300,
+                  fontWeight: "normal",
+                  textAlign: "center",
+                  fontSize: 35,
+                }}
+              >
+                Welcome,{" "}
+                <Text
+                  style={{ opacity: 0.9, fontWeight: "bold", fontSize: 30 }}
+                >
+                  {email || "Guest"}👋
+                </Text>
+              </Text>
+              <Pressable
+                disabled={isLoading}
+                onPress={() => {
+                  if (isLoading) return;
+                  setIsLoading(true);
+                  getDocs();
+                }}
+                style={{
+                  ...styles.button,
+                  width: 250,
+                  marginTop: 75,
+                  height: 60,
+                  opacity: isLoading ? 0.5 : 1,
+                }}
+              >
+                <Text style={styles.buttonText}>
+                  {isLoading ? "Loading.." : "Available Beds"}
+                </Text>
+              </Pressable>
+              <Pressable
+                disabled={isLoading}
+                onPress={() => {
+                  if (isLoading) return;
+                  setIsLoading(true);
+                  getDocs();
+                }}
+                style={{
+                  ...styles.button,
+                  opacity: isLoading ? 0.5 : 1,
+                  backgroundColor: "#f87171",
+                  width: 250,
+                  height: 60,
+                  marginTop: 15,
+                }}
+              >
+                <Text style={styles.buttonText}>{"Logout"}</Text>
+              </Pressable>
+            </>
+          )}
         </>
       )}
-    </View>
+    </Pressable>
   );
 }
 
 const styles = StyleSheet.create({
+  input: {
+    height: 47.5,
+    width: 250,
+    direction: "ltr",
+    textAlign: "left",
+    marginTop: 180,
+    borderRadius: 5,
+    backgroundColor: "#f0f0f0",
+    padding: 10,
+  },
   container: {
     flex: 1,
     backgroundColor: "#27272a",
@@ -200,38 +395,31 @@ const styles = StyleSheet.create({
   button: {
     alignItems: "center",
     justifyContent: "center",
-    paddingVertical: 20,
-    paddingHorizontal: 40,
-    borderRadius: 10,
-    elevation: 3,
+    height: 47.5,
+    width: 160,
+    borderRadius: 5,
     backgroundColor: "#3b82f6",
-    marginTop: 250,
-  },
-  loadingButton: {
-    alignItems: "center",
-    justifyContent: "center",
-    paddingVertical: 20,
-    paddingHorizontal: 40,
-    borderRadius: 10,
-    elevation: 3,
-    opacity: 0.2,
-    backgroundColor: "#3b82f6",
-    marginTop: 250,
+    marginTop: 30,
   },
   buttonText: {
-    fontSize: 32,
+    fontSize: 22,
     fontWeight: "bold",
-    color: "white",
+    color: "#f0f0f0",
+  },
+  buttonText: {
+    fontSize: 22,
+    fontWeight: "bold",
+    color: "#f0f0f0",
   },
   title: {
     fontSize: 50,
     fontWeight: "bold",
-    color: "white",
+    color: "#f0f0f0",
   },
   subtitle: {
     fontSize: 15,
     marginTop: 5,
-    color: "white",
+    color: "#f0f0f0",
     opacity: 0.8,
   },
   card: {
@@ -253,7 +441,7 @@ const styles = StyleSheet.create({
   },
   cardText: {
     fontSize: 15,
-    color: "white",
+    color: "#f0f0f0",
   },
   hospital: {
     fontSize: 19,
